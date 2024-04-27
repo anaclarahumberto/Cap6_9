f699bbea34f80c413154f174441d77beb405404b
anaclarahumberto <147638031+anaclarahumberto@users.noreply.github.com>
2024-04-27 16:11:09 -0300

    Update and rename ex9.14.py to exec9.14.py

--- ex9.14.py
+++
@@ -1,2 +0,0 @@
-"""Para encadear exceções em Python, você pode utilizar a cláusula raise dentro de um bloco except para lançar uma nova exceção em resposta a uma exceção capturada"""
-
---
+++ exec9.14.py
@@ -0,0 +1,1 @@
+print("Para encadear exceções em Python, você pode utilizar a cláusula raise dentro de um bloco except para lançar uma nova exceção em resposta a uma exceção capturada.")
