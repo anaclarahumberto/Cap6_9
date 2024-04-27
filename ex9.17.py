3ea2098091b5ac8860fbc4f01619772d3451c8d3
anaclarahumberto <147638031+anaclarahumberto@users.noreply.github.com>
2024-04-27 16:12:04 -0300

    Update and rename ex9.17.py to exec9.17.py

--- ex9.17.py
+++
@@ -1,6 +0,0 @@
-"""
-
-Quando você executa chamadas assíncronas dentro de blocos try...except, deve usar await para aguardar a conclusão dessas chamadas. Isso permite que você capture e trate exceções assíncronas 
-
-
-"""
---
+++ exec9.17.py
@@ -0,0 +1,1 @@
+print("Quando você executa chamadas assíncronas dentro de blocos try...except, deve usar await para aguardar a conclusão dessas chamadas. Isso permite que você capture e trate exceções assíncronas.")
