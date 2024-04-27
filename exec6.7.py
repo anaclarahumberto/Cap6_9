9cfd2673380a1632d6d860f8e5a432968715280d
anaclarahumberto <147638031+anaclarahumberto@users.noreply.github.com>
2024-04-27 15:16:26 -0300

    Create exec6.7.py

---
+++ exec6.7.py
@@ -0,0 +1,6 @@
+def encontrar_maior(lista):
+    return max(lista)
+
+numeros = [1, 4, 7, 23, 26, 200, 18, 825, 17]
+maior_valor = encontrar_maior(numeros)
+print("O maior valor da lista é:", maior_valor)
