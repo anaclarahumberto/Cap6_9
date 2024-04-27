366695e010f7d87cb5c69dea09adc152422b4cb5
anaclarahumberto <147638031+anaclarahumberto@users.noreply.github.com>
2024-04-27 15:13:52 -0300

    Update exec6.6.py

--- exec6.6.py
+++ exec6.6.py
@@ -1,8 +1,8 @@
-def adicionar_elemento(lista):
-    lista.append("novo elemento")
-
-minha_lista = []
-
-adicionar_elemento(minha_lista)
-
-print(minha_lista)
+def adicionar_elemento(lista, elemento):
+    lista.append(elemento)
+    print("Elemento adicionado com sucesso!")
+    print("Lista atualizada:", lista)
+    
+minha_lista = [1, 2, 3]
+novo_elemento = 4
+adicionar_elemento(minha_lista, novo_elemento)
