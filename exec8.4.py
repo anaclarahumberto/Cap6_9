3f98ed2ebdcb1502252c2c528e153071dacf0cd3
anaclarahumberto <147638031+anaclarahumberto@users.noreply.github.com>
2024-04-27 15:46:43 -0300

    Update exec8.4.py

--- exec8.4.py
+++ exec8.4.py
@@ -9,16 +9,16 @@ class Carro:
        return self.velocidade
 
     def frear(self, segundos):
-        desaceleracao = 5  # m/s^2
+        desaceleracao = 6  # m/s^2
         self.velocidade -= desaceleracao * segundos
         if self.velocidade < 0:
             self.velocidade = 0
             return self.velocidade
         
-carro1 = Carro(20)
+carro1 = Carro(30)
 
 print("Velocidade inicial:", carro1.velocidade)
-carro1.acelerar(5)  # Acelerando por 5 segundos
+carro1.acelerar(6)  # Acelerando por 6 segundos
 print("Velocidade após acelerar:", carro1.velocidade)
-carro1.frear(3)     # Freando por 3 segundos
+carro1.frear(4)     # Freando por 4 segundos
 print("Velocidade após frear:", carro1.velocidade)
