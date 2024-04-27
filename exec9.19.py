842645d7e32e60bc173f45a8a040bcb38c82eb98
anaclarahumberto <147638031+anaclarahumberto@users.noreply.github.com>
2024-04-27 16:13:41 -0300

    exec9.19.py

---
+++ exec9.19.py
@@ -0,0 +1,1 @@
+print("Dentro do próprio módulo pode se usar blocos try...except para capturar e lidar com exceções específicas que possam ocorrer durante a execução do código. Se uma exceção não puder ser tratada localmente, você pode optar por propagá-la para o código que chamou o módulo. Gerar arquivos de log.")
